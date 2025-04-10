c187546aed1633080142ad0ec4eb61090e719b74
Eyeo <tsrobertson2@bsu.edu>
2025-04-10 12:23:41 -0400

    PlayTest 2?

--- cryptidzoogodot/Assets/Scripts/fake_mothman.gd
+++ cryptidzoogodot/Assets/Scripts/fake_mothman.gd
@@ -9,7 +9,7 @@ var highlightMat = preload("res://Assets/Materials/fakeMothmanHighlight.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-
+	$MothManNoises.play(0)
 	$FakemothmanCol/FakemothmanMesh.set_surface_override_material(0, naturalMat)
 
 
@@ -19,12 +19,11 @@ func _process(delta: float) -> void:
 
 func highlight():
 	print("Switching")
-	var animName = "move" + str(animNum)
+	
 	$FakemothmanCol/FakemothmanMesh.set_surface_override_material(0, highlightMat);
 	$HighlightTimer.start()
-	$MothManAnims.play(animName)
-	animNum += 1
-	print("animNum: " + str(animNum) + "\nanimName: " + animName)
+	
+	
 
 
 func _on_highlight_timer_timeout() -> void:
@@ -33,15 +32,17 @@ func _on_highlight_timer_timeout() -> void:
 
 
 func _on_listening_radius_body_entered(body: Node3D) -> void:
-	print("Entered")
-	if body.is_in_group("Character"):
-		print("Character entered")
-		if body.velocity.length() >= 5:
-			print("Velocity met")
 
-	if body.is_in_group("Character") && body.velocity.length() >= 5 && animNum >= 1:
-		var animName = "move" + str(animNum)
+	if body.is_in_group("Character") && body.velocity.length() >= 5 && animNum > 1:
 		animNum -= 0.5
+		var animName = "move" + str(animNum)
+		print("Anim playing: " + str(animName))
 		$MothManAnims.play(animName)
 		animNum -= 0.5
 		
+
+func interact():
+	var animName = "move" + str(animNum)
+	$MothManAnims.play(animName)
+	animNum += 1
+	print("animNum: " + str(animNum) + "\nanimName: " + animName)
