83ce13e17eed97f9e2f29a738a5359038fd6fc4f
Tetrixstreams <122379072+Tetrixstreams@users.noreply.github.com>
2025-04-22 13:05:05 -0400

    changes

--- cryptidzoogodot/Assets/Scripts/fake_mothman.gd
+++ cryptidzoogodot/Assets/Scripts/fake_mothman.gd
@@ -10,7 +10,7 @@ var highlightMat = preload("res://Assets/Materials/fakeMothmanHighlight.tres")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$MothManNoises.play(0)
-	$MothmanFBX/Skeleton3D/Body.set_surface_override_material(0, naturalMat)
+	$FakemothmanCol/FakemothmanMesh.set_surface_override_material(0, naturalMat)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,8 +19,6 @@ func _process(delta: float) -> void:
 
 func highlight():
 	print("Switching")
-	
-	$FakemothmanCol/MothmanFBX/Skeleton3D/Body.set_surface_override_material(0, highlightMat)
 	
 	$FakemothmanCol/FakemothmanMesh.set_surface_override_material(0, highlightMat);
 	$HighlightTimer.start()
