d139c58a927924f19515b8a37e8da00cccef4b31
Quillen09 <kristaclanton0@gmail.com>
2025-09-02 15:37:43 -0400

    remove empty functions and put notes in folder

--- cryptidzoogodot/Assets/Scripts/StartScreenControl.gd
+++ cryptidzoogodot/Assets/Scripts/StartScreenControl.gd
@@ -1,16 +1,5 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_game_start_button_pressed() -> void: #Start
 	$ButtonPressNoise.play(0)
 	await get_tree().create_timer($ButtonPressNoise.stream.get_length())
