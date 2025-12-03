3f7ffb6be5fdf4ab701256ddfabefa47269a991c
Brayden Fairchild <153120388+brfairchild@users.noreply.github.com>
2025-12-03 14:31:37 -0500

    Trap1 animation

--- cryptidzoogodot/Wendigo Level/WendigoCharacter/wendigo_ai.gd
+++ cryptidzoogodot/Wendigo Level/WendigoCharacter/wendigo_ai.gd
@@ -24,7 +24,7 @@ var current_trap: Area3D = null
 @onready var ray_right: RayCast3D = $RayRight
 @onready var player_detection_area: Area3D = $PlayerDetectionArea
 @onready var trap_detection_area: Area3D = $TrapDetectionArea
-@onready var wendigoPlayer = get_node("/root/WendigoAnims")
+@onready var wendigoPlayer: AnimationPlayer = $"../WendigoAnims"
 @onready var activated5 = false
 @onready var activated2 = false
 @onready var activated3 = false
@@ -181,27 +181,28 @@ func _on_trap_detection_area_body_entered(body: Node3D) -> void:
 
 func _on_trap_2_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Enemies") and activated5 == true:
-		#wendigoPlayer.play("trap1")
+		wendigoPlayer.play("wendTrap1")
 		print("Trapped")
 		activated2 = false
 
 
 func _on_trap_3_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Enemies") and activated5 == true:
-		#wendigoPlayer.play("trap1")
+		wendigoPlayer.play("wendTrap1")
+		state = State.TRAPPED
 		print("Trapped")
 		activated3 = false
 
 
 func _on_trap_4_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Enemies") and activated5 == true:
-		#wendigoPlayer.play("trap1")
+		wendigoPlayer.play("wendTrap1")
 		print("Trapped")
 		activated4 = false
 
 func _on_trap_5_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Enemies") and activated5 == true:
-		#wendigoPlayer.play("trap1")
+		wendigoPlayer.play("wendTrap1")
 		print("Trapped")
 		activated5 = false
 
