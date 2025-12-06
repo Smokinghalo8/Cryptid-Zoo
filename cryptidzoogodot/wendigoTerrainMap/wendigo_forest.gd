1ccb8e00f7ddb2a194713cf53e23aad5ed98d542
Alexander Murray <alexander.murray@bsu.edu>
2025-12-06 14:00:55 -0500

    Fix double audio playing at start, change project stretch mode to ignore to remove bottom screen glitch
    also made the wendigo screech text have a tornado-y effect for flair.

--- cryptidzoogodot/wendigoTerrainMap/wendigo_forest.gd
+++ cryptidzoogodot/wendigoTerrainMap/wendigo_forest.gd
@@ -5,6 +5,8 @@ extends Node3D
 @onready var animPlayer : AnimationPlayer = $LevelAnimations
 @onready var minimap = $Ui/Minimap
 
+var has_visited_forest = false
+
 func _ready() -> void:
 	Global.frozen = true
 	$MainLevelMusic.play(0.0)
@@ -31,40 +33,42 @@ func _process(delta: float) -> void:
 
 
 func _on_forest_bound_1_body_entered(body: Node3D) -> void:
-	#await DialogueManager.show_dialogue_balloon(dialogue_resource, "EnterDarkForest").finished
-	pass
+	if body.is_in_group("Character") and not has_visited_forest:
+		await DialogueManager.show_dialogue_balloon(dialogue_resource, "EnterDarkForest").finished
+		has_visited_forest = true
 
 
 func _on_forest_bound_2_body_entered(body: Node3D) -> void:
-	#await DialogueManager.show_dialogue_balloon(dialogue_resource, "EnterDarkForest").finished
-	pass # Replace with function body.
+	if body.is_in_group("Character") and not has_visited_forest:
+		await DialogueManager.show_dialogue_balloon(dialogue_resource, "EnterDarkForest").finished
+		has_visited_forest = true
 
 #Actual trap 4
 func _on_trap_2_activated() -> void:
 	#Trap 4 complete
-	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap4Complete").finished
 	animPlayer.play("trap4Active")
+	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap4Complete").finished
 	minimap.objective = $Trap5
 
 #Actual trap 3
 func _on_trap_3_activated() -> void:
 	#Trap 3 complete
-	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap3Complete").finished
 	animPlayer.play("trap3Active")
+	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap3Complete").finished
 	minimap.objective = $Trap2
 
 #Actual trap 2
 func _on_trap_4_activated() -> void:
 	#Trap 2 complete
-	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap2Complete").finished
 	animPlayer.play("trap2Active")
+	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap2Complete").finished
 	minimap.objective = $Trap3
 
 #Actual trap 1
 func _on_trap_5_activated() -> void:
 	#Trap 1 complete
-	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap1Complete").finished
 	animPlayer.play("trap1Active")
+	await DialogueManager.show_dialogue_balloon(dialogue_resource, "Trap1Complete").finished
 	minimap.objective = $Trap4
 	
 
