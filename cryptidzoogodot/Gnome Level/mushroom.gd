7e45511e56f253e260c90f2e25ca6aeac164a0a9
Quillen09 <kristaclanton0@gmail.com>
2026-01-07 14:50:11 -0500

    create mushrooms

---
+++ cryptidzoogodot/Gnome Level/mushroom.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+# export either a boolean or a 1 to set a mushroom as poison/psychedelic so it can potentially
+# set randomly in the future
+#all mushrooms use this script
+#look up screen shaders for a psychedelic effect
