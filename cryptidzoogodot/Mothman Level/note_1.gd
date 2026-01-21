612d5a9b8d5b810e5a0ed1b9d5ae34b76b34931b
Alexander Murray <alexander.murray@bsu.edu>
2026-01-21 18:09:36 -0500

    Update the rest of the newspaper pop ups
    All Mothman newspapers now have the pop up animation with correct textures. Removed the old Wendigo newspaper textures i was using for testing.

--- cryptidzoogodot/Mothman Level/note_1.gd
+++ cryptidzoogodot/Mothman Level/note_1.gd
@@ -5,10 +5,6 @@ extends StaticBody3D
 #func interact():
 	#DialogueManager.show_dialogue_balloon(dialogue_resource, "Note1")
 
-var original_pos: Vector3
-var original_rot: Vector3
-var is_inspecting: bool = false
-
 func interact():
 	$"../Ui/Newspapers/NpMothman1".visible = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -16,8 +12,7 @@ func interact():
 	#insert dialogue here
 	
 
-func _on_button_pressed() -> void:
+func _on_np_mothman_1_button_pressed() -> void:
 	get_tree().paused = false
 	$"../Ui/Newspapers/NpMothman1".visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
--- cryptidzoogodot/Mothman Level/note_2.gd
+++ cryptidzoogodot/Mothman Level/note_2.gd
@@ -5,40 +5,14 @@ extends StaticBody3D
 #func interact():
 	#DialogueManager.show_dialogue_balloon(dialogue_resource, "Note2")
 
-var original_pos: Vector3
-var original_rot: Vector3
-var is_inspecting: bool = false
+func interact():
+	$"../Ui/Newspapers/NpMothman2".visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().paused = true
+	#insert dialogue here
 
-func interact(anchor: Marker3D):
-	if anchor == null: return
-		
-	if not is_inspecting:
-		start_inspection(anchor)
-	else:
-		stop_inspection()
-
-func start_inspection(anchor: Marker3D):
-	is_inspecting = true
-	# Store the original position and rotation separately
-	original_pos = global_position
-	original_rot = global_rotation
+func _on_np_mothman_2_button_pressed() -> void:
+	get_tree().paused = false
+	$"../Ui/Newspapers/NpMothman2".visible = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
-	
-	# Tween Position
-	tween.tween_property(self, "global_position", anchor.global_position, 0.5)
-	
-	# Tween Rotation to match the anchor
-	tween.tween_property(self, "global_rotation", anchor.global_rotation, 0.5)
-	
-	# If it's still backwards, uncomment the line below to flip it 180 degrees:
-	tween.tween_property(self, "rotation_degrees:y", anchor.rotation_degrees.y + 180, 0.5)
-
-func stop_inspection():
-	is_inspecting = false
-	
-	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
-	
-	# Return to original world position and rotation
-	tween.tween_property(self, "global_position", original_pos, 0.5)
-	tween.tween_property(self, "global_rotation", original_rot, 0.5)
