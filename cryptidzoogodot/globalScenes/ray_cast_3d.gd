4956928732973fa4be40cde01d0463e7e9286352
Alexander Murray <alexander.murray@bsu.edu>
2026-01-21 14:38:54 -0500

    added mothman newspapers

--- cryptidzoogodot/globalScenes/ray_cast_3d.gd
+++ cryptidzoogodot/globalScenes/ray_cast_3d.gd
@@ -1,6 +1,7 @@
 extends RayCast3D
 
 var intText
+@export var inspect_anchor: Marker3D
 
 func _ready():
 	intText = get_node("/root/" + get_tree().current_scene.name + "/Ui/intText")
@@ -9,12 +10,18 @@ func _ready():
 func _process(delta: float) -> void:
 	if is_colliding():
 		var target = get_collider()
-		if target != null:
-			if target.has_method("interact"):
-				if target.is_in_group("Traps"):
-					return
-				intText.visible = true
-				if Input.is_action_just_pressed("interact"):
+		if target != null and target.has_method("interact"):
+			if target.is_in_group("Traps"):
+				return
+				
+			intText.visible = true
+			if Input.is_action_just_pressed("interact"):
+				if target.is_in_group("newspaper"):
+					if inspect_anchor != null:
+						target.interact(inspect_anchor)
+					else:
+						print("ERROR: no anchor set")
+				else:
 					target.interact()
 			else:
 				intText.visible = false
