f186d311015a3059850624f955ba5155263632d3
Eyeo <tsrobertson2@bsu.edu>
2026-01-21 15:12:29 -0500

    First Newspaper Popup
    Coded the newspaper interaction so that it pops up on your screen

--- cryptidzoogodot/Mothman Level/note_1.gd
+++ cryptidzoogodot/Mothman Level/note_1.gd
@@ -9,36 +9,15 @@ var original_pos: Vector3
 var original_rot: Vector3
 var is_inspecting: bool = false
 
-func interact(anchor: Marker3D):
-	if anchor == null: return
-		
-	if not is_inspecting:
-		start_inspection(anchor)
-	else:
-		stop_inspection()
+func interact():
+	$"../Ui/Newspapers/NpMothman1".visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().paused = true
+	#insert dialogue here
+	
 
-func start_inspection(anchor: Marker3D):
-	is_inspecting = true
-	# Store the original position and rotation separately
-	original_pos = global_position
-	original_rot = global_rotation
+func _on_button_pressed() -> void:
+	get_tree().paused = false
+	$"../Ui/Newspapers/NpMothman1".visible = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
-	
-	# Tween Position
-	tween.tween_property(self, "global_position", anchor.global_position, 0.5)
-	
-	# Tween Rotation to match the anchor
-	tween.tween_property(self, "global_rotation", anchor.global_rotation, 0.5)
-	
-	# If it's still backwards, uncomment the line below to flip it 180 degrees:
-	tween.tween_property(self, "rotation_degrees:y", anchor.rotation_degrees.y + 180, 0.5)
-
-func stop_inspection():
-	is_inspecting = false
-	
-	var tween = create_tween().set_parallel(true).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
-	
-	# Return to original world position and rotation
-	tween.tween_property(self, "global_position", original_pos, 0.5)
-	tween.tween_property(self, "global_rotation", original_rot, 0.5)
--- cryptidzoogodot/globalScenes/ray_cast_3d.gd
+++ cryptidzoogodot/globalScenes/ray_cast_3d.gd
@@ -13,17 +13,10 @@ func _process(delta: float) -> void:
 		if target != null and target.has_method("interact"):
 			if target.is_in_group("Traps"):
 				return
-				
 			intText.visible = true
 			if Input.is_action_just_pressed("interact"):
-				if target.is_in_group("newspaper"):
-					if inspect_anchor != null:
-						target.interact(inspect_anchor)
-					else:
-						print("ERROR: no anchor set")
-				else:
-					target.interact()
-			else:
-				intText.visible = false
+				target.interact()
+		else:
+			intText.visible = false
 	else:
 		intText.visible = false
