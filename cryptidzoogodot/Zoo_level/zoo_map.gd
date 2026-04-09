896c4e90fa4c929a9460082ddfbeb98df4e58311
Eyeo <tsrobertson2@bsu.edu>
2026-04-09 19:10:55 -0400

    Fixing imports and whatnot

---
+++ cryptidzoogodot/Zoo_level/zoo_map.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+func _process(delta: float) -> void:
+	$Ui/SprintBar.value = Global.stamina
+	
+	if Global.stamina < 100:
+		$Ui/SprintBar.visible = true
+	if Global.stamina == 100:
+		$Ui/SprintBar.visible = false
+		
+		
